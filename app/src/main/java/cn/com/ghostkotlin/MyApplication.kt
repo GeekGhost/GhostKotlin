8db54c32b79c52ea14e45ceb23c3f1bb19cb47b8
caopeng <caopeng000@qq.com>
2018-07-03 10:56:41 +0800

    feat:新增gradle配置文件

---
+++ app/src/main/java/cn/com/ghostkotlin/MyApplication.kt
@@ -0,0 +1,13 @@
+package cn.com.ghostkotlin
+
+/**
+ * <pre>
+ *     @author : caopeng
+ *     e-mail : caopeng@qq.com
+ *     time   : 2018/07/02
+ *     desc   :
+ *     version: 1.0
+ * </pre>
+ */
+class MyApplication {
+}
