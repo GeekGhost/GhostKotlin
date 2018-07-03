4a4d7caa9ad1229b7e62efa0b3cc7dc178d7bb4e
caopeng <caopeng000@qq.com>
2018-07-03 15:15:58 +0800

    feat:新增application类和屏幕管理类

--- app/src/main/java/cn/com/ghostkotlin/MyApplication.kt
+++ app/src/main/java/cn/com/ghostkotlin/MyApplication.kt
@@ -1,5 +1,18 @@
 package cn.com.ghostkotlin
 
+import android.app.Activity
+import android.app.Application
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import cn.com.ghostkotlin.utils.DisplayManager
+import com.orhanobut.logger.AndroidLogAdapter
+import com.orhanobut.logger.Logger
+import com.orhanobut.logger.PrettyFormatStrategy
+import com.squareup.leakcanary.LeakCanary
+import com.squareup.leakcanary.RefWatcher
+import kotlin.properties.Delegates
+
 /**
  * <pre>
  *     @author : caopeng
@@ -9,5 +22,81 @@ package cn.com.ghostkotlin
  *     version: 1.0
  * </pre>
  */
-class MyApplication {
+class MyApplication : Application() {
+    private var refWatcher: RefWatcher? = null
+
+    companion object {
+
+        private val TAG = "MyApplication"
+
+        var context: Context by Delegates.notNull()
+            private set
+
+        fun getRefWatcher(context: Context): RefWatcher? {
+            val myApplication = context.applicationContext as MyApplication
+            return myApplication.refWatcher
+        }
+
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        context = applicationContext
+        refWatcher = setupLeakCanary()
+        initConfig()
+        DisplayManager.init(this)
+        registerActivityLifecycleCallbacks(mActivityLifecycleCallbacks)
+    }
+
+    private fun setupLeakCanary(): RefWatcher {
+        return if (LeakCanary.isInAnalyzerProcess(this)) {
+            RefWatcher.DISABLED
+        } else {
+            LeakCanary.install(this)
+        }
+    }
+
+    private fun initConfig() {
+        val formatStrategy = PrettyFormatStrategy.newBuilder()
+                .showThreadInfo(false)
+                .methodCount(0)
+                .methodOffset(7)
+                .tag("ghost")
+                .build()
+        Logger.addLogAdapter(object : AndroidLogAdapter(formatStrategy) {
+            override fun isLoggable(priority: Int, tag: String?): Boolean {
+                return BuildConfig.DEBUG
+            }
+        })
+    }
+
+    private val mActivityLifecycleCallbacks = object : Application.ActivityLifecycleCallbacks {
+        override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
+            Log.d(TAG, "onCreated: " + activity.componentName.className)
+        }
+
+        override fun onActivityStarted(activity: Activity) {
+            Log.d(TAG, "onStart: " + activity.componentName.className)
+        }
+
+        override fun onActivityResumed(activity: Activity) {
+
+        }
+
+        override fun onActivityPaused(activity: Activity) {
+
+        }
+
+        override fun onActivityStopped(activity: Activity) {
+
+        }
+
+        override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {
+
+        }
+
+        override fun onActivityDestroyed(activity: Activity) {
+            Log.d(TAG, "onDestroy: " + activity.componentName.className)
+        }
+    }
 }
