e52291a0de743a581f1d1756e6db008179f180da
caopeng <caopeng000@qq.com>
2018-07-04 16:39:23 +0800

    feat:新增加载图片框架

---
+++ app/src/main/java/cn/com/ghostkotlin/glide/ImageLoaderUtils.kt
@@ -0,0 +1,19 @@
+package cn.com.ghostkotlin.glide
+
+/**
+ * <pre>
+ *     @author : caopeng
+ *     e-mail : caopeng@qq.com
+ *     time   : 2018/07/04
+ *     desc   :
+ *     version: 1.0
+ * </pre>
+ */
+object ImageLoaderUtils{
+    init {
+
+    }
+
+
+
+}
